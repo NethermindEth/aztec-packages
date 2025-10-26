17da99a425e8f0c682221b80086db8d155008bfa
Charlie Lye <5764343+charlielye@users.noreply.github.com>
2025-10-26 21:46:17 +0000

    fix

--- yarn-project/scripts/run_test.sh
+++ yarn-project/scripts/run_test.sh
@@ -12,6 +12,8 @@ name=$test
 
 cd ../$dir
 
+export RAYON_NUM_THREADS=1
+export HARDWARE_CONCURRENCY=${CPUS:-16}
 export LOG_LEVEL=${LOG_LEVEL:-info}
 exec node --no-warnings --experimental-vm-modules --loader @swc-node/register \
   ../node_modules/.bin/jest --forceExit --runInBand $test "$@"
